636b27d595ffdc789a5f6ca6a117674a08d69eaf
LloydLion <LloydLion2211@gmail.com>
2020-07-28 03:22:53 +0300

    Basic Functional

--- MassiveEmailSender/Config.cs
+++ MassiveEmailSender/Config.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +11,54 @@ namespace MassiveEmailSender
 {
 	class Config
 	{
+		public const string DefaultConfigFileName = "emal_config.json";
+		public const string DefaultEmailsTxtFileName = "emails.txt";
+
+
+		public MailAddress SenderAdress { get; private set; }
+		public string SenderPassword { get; private set; }
+		public string SmtpServerAddress { get; private set; }
+		public MailAddress[] TargetAdresses { get; private set; }
+
+
+		public static Config LoadConfigFromFile(string fileName)
+		{
+			return LoadConfigFromString(File.ReadAllText(fileName));
+		}
+
+		public static Config LoadConfigFromString(string value)
+		{
+			var model = JsonConvert.DeserializeObject<ParseModel>(value);
+
+			return new Config(new MailAddress(model.SenderAdress),
+				new MailAddress[0], model.SenderPassword, model.SmtpServerAddress);
+		}
+
+		/*public string ConvertToString()
+		{
+			return JsonConvert.SerializeObject(this);
+		}*/
+
+		public void SetAddressesFromTxtFile(string path)
+		{
+			TargetAdresses = File.ReadAllText(path).Replace("\r\n", "\u1234")
+				.Split('\u1234').Select((s) => new MailAddress(s)).ToArray();
+		}
+
+		public Config(MailAddress sender, MailAddress[] target, string password, string smtpServerAddress)
+		{
+			SenderAdress = sender;
+			TargetAdresses = target;
+			SenderPassword = password;
+			SmtpServerAddress = smtpServerAddress;
+		}
+
+
+		private class ParseModel
+		{
+			public string SenderAdress { get; set; }
+			public string SenderPassword { get; set; }
+			public string SmtpServerAddress { get; set; }
+		}
 	}
 }
--- MassiveEmailSender/Program.cs
+++ MassiveEmailSender/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +11,50 @@ namespace MassiveEmailSender
 {
 	class Program
 	{
+		public const string DefaultEmailContentFileName = "content.html";
+
+
 		static void Main(string[] args)
 		{
+			Config config = Config.LoadConfigFromFile(Config.DefaultConfigFileName);
+			config.SetAddressesFromTxtFile(Config.DefaultEmailsTxtFileName);
+
+			SendAll(config, File.ReadAllText(DefaultEmailContentFileName));
+
+			Console.WriteLine("Program work endded. Press any key to close");
+			Console.ReadKey();
+		}
+
+		static void SendAll(Config config, string text)
+		{
+			var sender = new SmtpClient(config.SmtpServerAddress)
+			{
+				EnableSsl = true,
+				UseDefaultCredentials = false,
+				Credentials = new NetworkCredential(config.SenderAdress.Address, config.SenderPassword)
+			};
+
+
+
+			Console.WriteLine("Sending from " + config.SenderAdress.Address);
+			Console.WriteLine();
+
+			for (int i = 0; i < config.TargetAdresses.Length; i++)
+			{
+				var target = config.TargetAdresses[i];
+				var message = new MailMessage(config.SenderAdress, target)
+				{
+					Subject = " ",
+					IsBodyHtml = true,
+					Body = text
+				};
+
+				Console.Write("Sending to " + target.Address);
+				sender.Send(message);
+				Console.WriteLine(" ... OK!");
+			}
+
+			Console.WriteLine(new string('-', 40));
 		}
 	}
 }
