32b3896db3a1bb350b8acf5e64255ebe3730bacd
LloydLion <LloydLion2211@gmail.com>
2020-08-01 13:22:27 +0300

    Console logging

--- MassiveEmailSender/Program.cs
+++ MassiveEmailSender/Program.cs
@@ -18,31 +18,63 @@ namespace MassiveEmailSender
 
 		static void Main(string[] args)
 		{
+			Console.WriteLine("Massive Email Sender Utility");
+			Console.Write("Check setting before start program ");
+
+			for (int i = 0; i < 6; i++)
+			{
+				Thread.Sleep(500);
+				Console.Write('.');
+			}
+
+			Console.WriteLine();
+			Console.WriteLine("Press any key to start utility");
+			Console.ReadKey();
+
+			Console.WriteLine();
+			Console.WriteLine("Work started");
+
 			try
 			{
-
+				Console.Write("Reading config ... ");
 				Config config = Config.LoadConfigFromFile(Config.DefaultConfigFileName);
 				config.SetAddressesFromTxtFile(Config.DefaultEmailsTxtFileName);
+				Console.WriteLine("OK!");
 
+				Console.Write("Reading message ... ");
 				MailMessage message = new MailMessage
 				{
 					IsBodyHtml = true,
 					Body = File.ReadAllText(DefaultEmailContentFileName),
 					Subject = ""
 				};
+				Console.WriteLine("OK!");
 
+				Console.Write("Creating client ... ");
 				MailSender sender = new MailSender(config.SenderAddress, config.SenderPassword,
 					config.SmtpServerAddress);
+				Console.WriteLine("OK!");
 
-				sender.SendToAll(config.TargetAdresses, message);
+				Console.Write("Sending emails ");
 
 
+				var task =
+					Task.Run(() => sender.SendToAll(config.TargetAdresses, message));
+
+				for (int i = 0; !task.IsCompleted; i++)
+				{
+					Thread.Sleep(1000);
+					Console.Write(".");
+				}
+
+				Console.WriteLine(" OK!");
 			}
 			catch(Exception ex)
 			{
 				Console.WriteLine("Unhandled Exception: ");
-				Console.WriteLine(ex.Message);
-				Console.WriteLine(ex.StackTrace);
+				Console.WriteLine("\tMessage: " + ex.Message);
+				Console.WriteLine("\tStackTrace: " + ex.StackTrace);
+				Console.WriteLine("\t" + "Source: " + ex.Source);
 			}
 
 			Console.WriteLine(new string('-', 30));
